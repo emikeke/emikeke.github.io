162d8c1a8b8c67aec53d91b4734c091bf8066a68
Emily K <emily.ke@hotmail.com>
2021-12-09 15:09:34 -0500

    responsive for small phones for projects page

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 $(window).on("load", function(){
   const swiper = new Swiper(".swiper", {
+    cssMode: true,
     loop: true,
-  
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
     keyboard: true,
   });
